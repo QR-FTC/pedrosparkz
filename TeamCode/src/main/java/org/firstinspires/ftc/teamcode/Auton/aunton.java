93f98003b838bd8f99f8aaa9550cf1e62f99085e
Saumyaa <saumyaa.mangla@gmail.com>
2025-11-08 10:05:27 -0800

    Update aunton.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auton/aunton.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auton/aunton.java
@@ -19,7 +19,8 @@ import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
             private Follower follower;
             private Timer pathTimer, actionTimer, opmodeTimer;
             private int pathState;
-            private final Pose startPose = new Pose(28.5, 128, Math.toRadians(180)); // Start Pose of our robot.
+            private final Pose startPose = new Pose(56, 8, Math.toRadians(90)); // Start Pose of our robot.
+    private final Pose startPose2 = new Pose(24, 120, Math.toRadians(135));
             private final Pose scorePose = new Pose(60, 85, Math.toRadians(135)); // Scoring Pose of our robot. It is facing the goal at a 135 degree angle.
             private final Pose pickup1Pose = new Pose(37, 121, Math.toRadians(0)); // Highest (First Set) of Artifacts from the Spike Mark.
             private final Pose pickup2Pose = new Pose(43, 130, Math.toRadians(0)); // Middle (Second Set) of Artifacts from the Spike Mark.
