53f8d7dc1422d3c1bca0ea0439f7053bb0d4a490
Saumyaa <saumyaa.mangla@gmail.com>
2025-11-09 09:52:35 -0800

    Update autonmoving.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auton/autonmoving.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auton/autonmoving.java
@@ -39,7 +39,7 @@ public class autonmoving extends OpMode {
 
 
     //    private final Pose scorePose = new Pose(86, 105, Math.toRadians(45)); // Scoring Pose of our robot. It is facing the goal at a 135 degree angle.
-    private final Pose pickup1Posebeg = new Pose(50, 16, Math.toRadians(90)); // Highest (First Set) of Artifacts from the Spike Mark.
+    private final Pose pickup1Posebeg = new Pose(54, 16, Math.toRadians(90)); // Highest (First Set) of Artifacts from the Spike Mark.
 
 
     private Path scorePreload;
