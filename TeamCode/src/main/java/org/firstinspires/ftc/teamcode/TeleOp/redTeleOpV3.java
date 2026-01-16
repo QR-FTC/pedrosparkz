bb430097fe45950e1906373b17f524774143af64
amulyapasu <amulyapasu@gmail.com>
2026-01-15 20:20:25 -0800

    this code is the function for RPM

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/redTeleOpV3.java
@@ -0,0 +1,162 @@
+    package org.firstinspires.ftc.teamcode.TeleOp;
+
+    import com.bylazar.telemetry.PanelsTelemetry;
+    import com.bylazar.telemetry.TelemetryManager;
+    import com.pedropathing.follower.Follower;
+    import com.pedropathing.geometry.Pose;
+    import com.pedropathing.paths.HeadingInterpolator;
+    import com.pedropathing.paths.PathChain;
+    import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+    import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+    import com.qualcomm.robotcore.hardware.DcMotor;
+    import com.qualcomm.robotcore.hardware.DcMotorEx;
+    import com.qualcomm.robotcore.hardware.PIDFCoefficients;
+    import com.qualcomm.robotcore.util.ElapsedTime;
+
+    import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
+
+    import java.util.function.Supplier;
+
+    @TeleOp(name="redTeleOpV3")
+    public class redTeleOpV3 extends OpMode {
+        private int D;
+        private ElapsedTime SleepTimer = new ElapsedTime();
+        private DcMotorEx deposit;
+        private DcMotor intake;
+        private static Follower follower;
+        private ShooterCalculatons shooterCalculatons;
+        public static Pose startingPose; //See ExampleAuto to understand how to use this
+        private Supplier<PathChain> pathChain;
+        private TelemetryManager telemetryM;
+        private double slowModeMultiplier = 0.5;
+        private boolean automatedDrive = false;
+
+        @Override
+        public void init() {
+            shooterCalculatons = new ShooterCalculatons();
+            follower = Constants.createFollower(hardwareMap);
+            follower.setStartingPose(new Pose(36, 12, 90));
+            follower.update();
+            telemetryM = PanelsTelemetry.INSTANCE.getTelemetry();
+            pathChain = () -> follower.pathBuilder() //Lazy Curve Generation
+                    .setHeadingInterpolation(HeadingInterpolator.linearFromPoint(follower::getHeading, shooterCalculatons.getthetared(follower.getPose().getX(),follower.getPose().getY()), 0.8))
+                    .build();
+            intake = hardwareMap.get(DcMotor.class, "intake"); // EH Port 0
+            deposit = hardwareMap.get(DcMotorEx.class, "deposit"); // EH port 1
+
+            // Tuned vals for P and F
+            final double P = 342;
+            final double F = 14;
+            PIDFCoefficients pidfCoefficients = new PIDFCoefficients(P, 0, 0, F);
+            deposit.setPIDFCoefficients(DcMotorEx.RunMode.RUN_USING_ENCODER, pidfCoefficients);
+        }
+
+        @Override
+
+        public void start() {
+            //The parameter controls whether the Follower should use break mode on the motors (using it is recommended).
+            //In order to use float mode, add .useBrakeModeInTeleOp(true); to your Drivetrain Constants in Constant.java (for Mecanum)
+            //If you don't pass anything in, it uses the default (false)
+            follower.startTeleopDrive();
+        }
+
+        @Override
+        public void loop() {
+            SleepTimer.reset();
+            //Call this once per loop
+            follower.update();
+            if (!automatedDrive){
+            //Make the last parameter false for field-centric
+            follower.setTeleOpDrive(
+                    -gamepad1.left_stick_y,
+                    -gamepad1.left_stick_x,
+                    -gamepad1.right_stick_x,
+                    true // Robot Centric
+
+            );}
+
+            // When gamepad-1 right bumper is pressed run intake 1 and 2 motors
+            if (gamepad1.right_bumper) {
+                intake.setPower(0.8);
+            } else if (gamepad1.left_bumper) { // When Gamepad-1 left bumper is pressed reverse the intake motor
+                intake.setPower(-0.8);
+            } else { // if not stop the motor
+                intake.setPower(0.0);
+            }
+            //Automated PathFollowing
+            if (gamepad1.aWasPressed()) {
+                follower.followPath(pathChain.get());
+                automatedDrive = true;
+            }
+            //Stop automated following if the follower is done
+            if (automatedDrive && (gamepad1.bWasPressed() || !follower.isBusy())) {
+                follower.startTeleopDrive();
+                automatedDrive = false;
+            }
+            // When gamepad-2 right trigger is pressed start deposit motor.
+            if (gamepad2.right_trigger > 0.1) {
+                deposit.setPower(-0.75 * (gamepad2.right_trigger));
+            } else { // else stop the motor
+                deposit.setPower(0.0);
+            }
+
+            //deposit.setVelocity(getRPM(getDistance(), 35));
+            //telemetry.addData("Deposit Servo Position", servoDeposit.getPosition());
+            //telemetry.addData("Gate Servo Position", servoIntake.getPosition());
+            //telemetry.addData("Intake Power Left", intake_2.getPower());
+            //telemetry.addData("Intake Power Right", intake_3.getPower());
+            telemetry.addData("Deposit Power", deposit.getPower());
+            telemetry.addData("Current X pos", follower.getPose().getX());
+            telemetry.addData("Current Y Pos", follower.getPose().getY());
+            telemetry.addData("Current Heading", follower.getPose().getHeading());
+            telemetry.addData("Distance",getDistance());
+            telemetry.addData("Current RPM", getRPM(getDistance(), 35));
+            telemetry.update();
+
+        }
+
+        public double getDistance()
+        {
+            Pose pose = follower.getPose();
+            double targetX = 12; // 132
+            double targetY = 132; // 132
+            double distanceInches = Math.sqrt(Math.pow((pose.getX() - targetX), 2) + Math.pow(pose.getY() - targetY, 2));
+
+            // Converting the distance from inches to mm by multiplying by 25.4.
+            return distanceInches*2.54;
+        }
+
+
+        public double getRPM(double distance, double theta){
+
+            // Gravity in mm
+            double gravityMM = 980.694;
+            double targetHeight = 81.28; // 2.54 * (43 - 11 )
+            double thetaRadian = Math.toRadians(theta);
+
+            // G is Gravity in mm 9806.94
+            // d is horizontal distance from the goal to robot
+            // theta is the hood angle in degrees
+            // thetaRadian is the hood angle in radians
+            // targetHight is the height of the goal in mm
+
+            // v^2 = G*d^2/(2.cos(theta)^2(d.tan(theta)-targetHight)
+            double numerator = (gravityMM*distance*distance);
+            double denominator = (2* Math.pow(Math.cos(thetaRadian), 2)*(distance*Math.tan(thetaRadian)-targetHeight));
+
+            telemetry.addData("target Height", targetHeight);
+            telemetry.addData("Numerator", numerator);
+            telemetry.addData("Denominator", denominator);
+
+            if (denominator <=0) return 0;
+
+            double velocity = Math.sqrt( numerator / denominator);
+
+            telemetry.addData("velocity", velocity);
+
+            double flywheelRadius = 4.8;
+            return (60*velocity) / (2*Math.PI*flywheelRadius);
+
+        }
+
+    }
