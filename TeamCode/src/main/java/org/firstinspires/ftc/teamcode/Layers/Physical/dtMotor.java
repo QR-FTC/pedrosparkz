ac54961dca8a2fb404d61d9b6d444ebaea917cdd
Saumyaa <saumyaa.mangla@gmail.com>
2026-07-18 11:11:40 -0700

    Update dtMotor.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Layers/Physical/dtMotor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Layers/Physical/dtMotor.java
@@ -1,8 +1,7 @@
 package org.firstinspires.ftc.teamcode.Layers.Physical;
-
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.HardwareMap;
 
+import com.qualcomm.robotcore.hardware.HardwareMap;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 public class dtMotor {
