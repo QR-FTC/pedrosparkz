b391612f27e6ad0b70660e6fdd6b4d61f3521de0
Saumyaa <saumyaa.mangla@gmail.com>
2026-07-18 11:12:05 -0700

    n

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Layers/Physical/layermotor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Layers/Physical/layermotor.java
@@ -3,5 +3,8 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 public class layermotor {
+    DcMotor motor;
+    Telemetry telemetry;
+
     
 }
