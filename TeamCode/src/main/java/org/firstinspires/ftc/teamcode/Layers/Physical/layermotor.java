8d0a82dd3f8faeb55f29d5a02d68d93d55eda51f
Aadvik <aadvikgupta007@gmail.com>
2026-07-18 11:20:03 -0700

    layered architecture

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Layers/Physical/layermotor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Layers/Physical/layermotor.java
@@ -1,4 +1,6 @@
 package org.firstinspires.ftc.teamcode.Layers.Physical;
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
+
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
@@ -6,5 +8,22 @@ public class layermotor {
     DcMotor motor;
     Telemetry telemetry;
 
-    
+    public layermotor(Telemetry telemetry, HardwareMap hardwareMap, String name) {
+
+       motor=  hardwareMap.get(DcMotor.class, name);
+
+        this.telemetry = telemetry;
+    }
+
+    public void setPower(double power) {
+        motor.setPower(power);
+
+
+
+
+    }
+
+    public void stopMotor(){
+        motor.setPower(0);
+    }
 }
